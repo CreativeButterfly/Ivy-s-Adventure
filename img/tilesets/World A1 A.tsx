3b6a2fa46cde2277eb3d389266e2ee7704a72695
Claire Toman <flyingmoth9785@gmail.com>
2021-11-29 14:16:56 -0700

    begin transition to TileD

---
+++ img/tilesets/World A1 A.tsx
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="World A1" tilewidth="48" tileheight="48" tilecount="432" columns="24">
+ <image source="World_A1A.png" width="1152" height="864"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="8" duration="200"/>
+   <frame tileid="16" duration="200"/>
+   <frame tileid="8" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="9" duration="200"/>
+   <frame tileid="17" duration="200"/>
+   <frame tileid="9" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="10" duration="200"/>
+   <frame tileid="18" duration="200"/>
+   <frame tileid="10" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="11" duration="200"/>
+   <frame tileid="19" duration="200"/>
+   <frame tileid="11" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="12" duration="200"/>
+   <frame tileid="20" duration="200"/>
+   <frame tileid="12" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="13" duration="200"/>
+   <frame tileid="21" duration="200"/>
+   <frame tileid="13" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="14" duration="200"/>
+   <frame tileid="22" duration="200"/>
+   <frame tileid="14" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="15" duration="200"/>
+   <frame tileid="23" duration="200"/>
+   <frame tileid="15" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="200"/>
+   <frame tileid="32" duration="200"/>
+   <frame tileid="40" duration="200"/>
+   <frame tileid="32" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
